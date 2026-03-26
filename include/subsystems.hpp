c68a88e7ad16474d3661cc36e3691c91493f6944
SantiagoCarvajal2024 <santiagocarvajal2024@outlook.com>
2026-03-25 19:02:23 -0500

    First

---
+++ include/subsystems.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "EZ-Template/api.hpp"
+#include "api.h"
+using namespace pros;
+extern Drive chassis;
+inline Motor Intake_1(11);
+inline Motor Intake_2(12);
+inline ADIDigitalOut Doinker('A');
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
